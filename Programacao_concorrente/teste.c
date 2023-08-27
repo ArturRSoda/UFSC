214ae1acca897a8803035d52ec65dff1d3755eef
root <root@desktop-artur>
2023-08-27 18:01:05 -0300

    programacao concorrente

--- Programacao_concorrente/teste.c
+++ Programacao_concorrente/teste.c
@@ -1,32 +1,28 @@
 #include <stdio.h>
 #include <string.h>
 
+int f(int a) {
+	if (a % 2 != 0 && a < 20) {
+		return a;
+	} else if (&a) {
+		if (a < 0) return 2*a;
+	} else {
+		return 27;
+	}
+	return 31;
+}
+
+
 int main() {
 
-	int dataA[5][5];
-	int dataB[5][5];
-	int TAM = 5;
-
-	int count = 1;
-	for (int j = 0; j < 5; j++) {
-		for (int i = 0; i < 5; i++) {
-			dataA[i][j] = count;
-			count++;
-		}
-	}
-
-
-	for (int i = 0; i < TAM; i++)
-	  for (int j = 0; j < TAM; j++)
-		dataB[i*TAM+j] = dataA[i*TAM+j];
+	char s1[] = "HELLO";
+	char* s2 = "WORLD";
+	char* s3 = s2;
+	char *s4 = malloc(sizeof(char));
+	strcpy(s4, s3);
+	printf("%s %s\n", s1, s4);
 
 
 
-	for (int i = 0; i < 5; i++) {
-		for (int j = 0; j < 5; j++) {
-			printf("%d ", dataB[i][j]);
-		}
-		printf("\n");
-	}
 	return 0;
 }
