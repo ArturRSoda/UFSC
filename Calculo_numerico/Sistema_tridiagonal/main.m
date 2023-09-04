a6f101afd41fc488f71bfa49ef0b67700daa0aaa
root <root@desktop-artur>
2023-09-04 17:22:21 -0300

    Sistema tridiagonal calculo numerico

---
+++ Calculo_numerico/Sistema_tridiagonal/main.m
@@ -0,0 +1,31 @@
+A = [1 -1 0 0 0;
+	1 1 -1 0 0;
+	0 1 -1 1 0;
+	0 0 -1 1 1;
+	0 0 0 -1 2]
+
+B = [0; 1; 2; -1; -2]
+
+n = size(A, 1);
+T = zeros(n, 1);
+R = zeros(n, 1);
+D = zeros(n, 1);
+
+for (i = 1:n)
+	if (i == 1)
+		T(i) = NaN;
+	else
+		T(i) = A(i,(i-1));
+	end
+
+	if (i == n)
+		D(i) = NaN;
+	else
+		D(i) = A(i,(i+1));
+	end
+
+	R(i) = A(i,i);
+end
+
+
+X = tridiagonal(T, R, D, B)
