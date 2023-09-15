31a3ae5fc2eb7080b40422fc6fee5642af7f71e4
ARTUR <artursodar@gmail.com>
2023-09-15 15:13:21 -0300

    algoritmo de newton - calculo numerico

--- Calculo_numerico/Meus_Codigos/Algoritmo_Newton/localizacao.m
+++ Calculo_numerico/Meus_Codigos/Algoritmo_Newton/localizacao.m
@@ -1,15 +1,14 @@
 function xi = localizacao(f, dominio)
-	n_intervalos = 1000;
+	n_intervalos = 100;
 	h = (dominio(2) - dominio(1))/n_intervalos;
 	x = dominio(1):h:dominio(2);
 	y = f(x);
 
 	i_raiz = 0;
 	for (i = 1:(length(x)-1))
-		if ((y(i) * y(i+1)) <= 0)
+		if (((y(i) * y(i+1)) <= 0) && ((abs(y(i)) + abs(y(i+1)) < 0.1)))
 			i_raiz++;
 			xi(i_raiz) = (x(i) + x(i+1)) * 0.5;
 		end
 	end
-	xi
 end
--- Calculo_numerico/Meus_Codigos/Algoritmo_Newton/main.m
+++ Calculo_numerico/Meus_Codigos/Algoritmo_Newton/main.m
@@ -1,7 +1,12 @@
 
-f = @(x) x .* log(x) .- 1
 
-subdominio = [1e-4 10]
+%exemplo 1
+%f = @(x) x .* log(x) .- 1
+%subdominio = [1e-4 10]
+
+%exemplo 2
+f = @(x) x .* tan(x) .- 1
+subdominio = [-7 7]
 
 x = metodo_newton(f, subdominio)
 residuo = abs(f(x))
