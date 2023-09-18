f37a3e36b29e8d2e36566e6ec617c2b4d73ceaa2
root <root@desktop-artur>
2023-09-17 21:06:52 -0300

    Update algoritmo newton

--- Calculo_numerico/Meus_Codigos/Algoritmo_Newton/localizacao.m
+++ Calculo_numerico/Meus_Codigos/Algoritmo_Newton/localizacao.m
@@ -3,10 +3,11 @@ function xi = localizacao(f, dominio)
 	h = (dominio(2) - dominio(1))/n_intervalos;
 	x = dominio(1):h:dominio(2);
 	y = f(x);
+	limite = 0.1/h;
 
 	i_raiz = 0;
 	for (i = 1:(length(x)-1))
-		if (((y(i) * y(i+1)) <= 0) && ((abs(y(i)) + abs(y(i+1)) < 0.1)))
+		if (((y(i) * y(i+1)) <= 0) && ((abs(y(i)) + abs(y(i+1)) < limite)))
 			i_raiz++;
 			xi(i_raiz) = (x(i) + x(i+1)) * 0.5;
 		end
--- Calculo_numerico/Meus_Codigos/Algoritmo_Newton/metodo_newton.m
+++ Calculo_numerico/Meus_Codigos/Algoritmo_Newton/metodo_newton.m
@@ -3,12 +3,16 @@ function x = metodo_newton(f, dominio)
 	xi = localizacao(f, dominio);
 	delta_x = 1;
 	k = 0;
-	while ((abs(delta_x) > 1e-15) && (k < 100))
-		k += 1;
-		derivada_f = (f(xi + delta_x) - f(xi))/delta_x;
-		delta_x = -f(xi)/derivada_f;
-		x = xi + delta_x;
-		xi = x;
+
+
+	for (i = 1:length(xi))
+		while ((abs(delta_x) > 1e-15) && (k < 100))
+			k += 1;
+			derivada_f = (f(xi(i) + delta_x) - f(xi(i)))/delta_x;
+			delta_x = -f(xi(i))/derivada_f;
+			x(i) = xi(i) + delta_x;
+			xi(i) = x(i);
+		end
 	end
 end
 
