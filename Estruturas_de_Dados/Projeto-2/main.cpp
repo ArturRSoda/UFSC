abb02060fa34e9748c51db985583cc9b741ab303
root <root@desktop-artur>
2023-11-04 13:03:33 -0300

    projeto 2 - estruturas de dados

--- Estruturas_de_Dados/Projeto-2/main.cpp
+++ Estruturas_de_Dados/Projeto-2/main.cpp
@@ -1,23 +1,71 @@
-#include <cctype>
 #include <iostream>
-#include <locale>
+#include <fstream>
 #include <string>
 
 #include "trie_tree.h"
 
 using namespace std;
 
+
+void read_file(ifstream& file, structures::TrieTree<string>& tree);
+void search_tree(structures::TrieTree<string>& tree);
+
 int main() {
 
-	structures::TrieTree<string> T;
+	structures::TrieTree<string> tree;
+	string filename;
 
-	T.insert("artur", 100, 200);
-	T.insert("artur soda", 100, 200);
+    cin >> filename;  // entrada
 
-	string x = "Artur ";
-	cout << "tem -> " << T.contain(x) << endl;
-	cout << "prefix -> " << T.get_qtd_prefix(x);
+	ifstream file(filename);
 
+	read_file(file, tree);
+	search_tree(tree);
 
 	return 0;
+}
+
+void read_file(ifstream& file, structures::TrieTree<string>& tree) {
+	
+	string line;
+	string word;
+	int length;
+	int position = 0;
+
+	while (getline(file, line)) {
+		int end = line.find(']');
+		length = line.length();	
+		word = line.substr(1, end-1);
+
+		tree.insert(word, position, length);
+
+		position += length+1;
+	}
+
+}
+
+void search_tree(structures::TrieTree<string>& tree) {
+    string word;
+
+    while (1) {  
+        cin >> word;
+
+        if (word.compare("0") == 0) {
+            break;
+        }
+
+		if (!tree.contain(word)) {
+			cout << word << " is not prefix" << endl;
+		}
+		else {
+			int qtd_prefix = tree.get_qtd_prefix(word);
+			int position = tree.get_position(word);
+			int length = tree.get_length(word);
+
+			cout << word << " is prefix of " << qtd_prefix << " words" << endl;
+
+			if (length != 0)
+				cout << word << " is at (" << position << "," << length << ")" << endl;
+		}
+    }
 }
