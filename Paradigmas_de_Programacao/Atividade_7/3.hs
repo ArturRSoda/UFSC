dac7f0f995ff7e5d0b3dfb5a2642cdd5933555c9
ArturRSoda <artursodar@gmail.com>
2024-04-10 16:51:04 -0300

    paradigmas: update atividade 7

--- Paradigmas_de_Programacao/Atividade_7/3.hs
+++ Paradigmas_de_Programacao/Atividade_7/3.hs
@@ -4,6 +4,10 @@ class (Integral x) => MeuInt x where
     par :: x -> Bool
     impar :: x -> Bool
     primo :: x -> x -> Bool
+    mdc :: x -> x -> x
+    isNegative :: x -> Bool
+    (===) :: x -> x -> Bool
+    (==/) :: x -> x -> Bool
 
 
     bigger a b | (a > b) = a
@@ -23,6 +27,18 @@ class (Integral x) => MeuInt x where
               | ((mod a i) == 0) = False
               | otherwise = primo a (i+1)
 
+    mdc a b | ((mod (max a b) (min a b)) == 0) = min a b
+            | otherwise = mdc (min a b) (mod (max a b) (min a b))
+
+    a === b | (abs(a - b) <= 1) = True
+            | otherwise = False
+
+-- Retorna se um numero eh negativo
+    isNegative a = (a < 0)
+
+-- Operacao que verifica se a eh multiplo de b
+    a ==/ b = ((mod a b) == 0)
+
 instance MeuInt Int
 
 main = do
@@ -33,3 +49,9 @@ main = do
     print(impar (1::Int))
     print(primo (17::Int) (2::Int))
     print(primo (20::Int) (2::Int))
+    print(mdc (20:: Int) (16::Int))
+    print((2::Int) === (1::Int))
+    print((2::Int) === (10::Int))
+    print(isNegative (-1::Int))
+    print((20::Int) ==/ (5::Int))
+
