837f4e39a7d154d084e4dcfd75aaa0fbd1f00847
ArturRSoda <artursodar@gmail.com>
2024-04-10 21:27:16 -0300

    Paradigmas: update trabalho 1

--- Paradigmas_de_Programacao/Trabalho_1/Src/Solucao_Python/main.py
+++ Paradigmas_de_Programacao/Trabalho_1/Src/Solucao_Python/main.py
@@ -1,11 +1,39 @@
 from sys import argv
 
+class Position:
+    def __init__(self, value: int) -> None:
+        self.value: int = value
+        self.upBorderBlock: bool = False
+        self.downBorderBlock: bool = False
+        self.leftBorderBlock: bool = False
+        self.rightBorderBlock: bool = False
+
+def make_board(lines: list[str], n: int) -> list[list[Position]]:
+    board: list[list[Position]] = list()
+    tempList: list[Position] = list()
+
+    for i in range(0, (n*2)-1, 2):
+        tempList.clear()
+        for j in range(0, (n*2)-1, 2):
+            tempList.append(Position(int(lines[i][j]) if (lines[i][j] != "*") else -1))
+        board.append(tempList.copy())
+
+
+    return board
+
+
 def main() -> None:
     file: str = argv[1]
+    n: int = int(argv[2])
 
     f = open(file, "r")
-    print(f.read())
-
+    text: str = f.read()
     f.close()
+
+    board = make_board(text.split('\n'), n)
+    for i in range(n):
+        for j in range(n):
+            print(board[i][j].value, end=" ")
+        print()
 
 main()
