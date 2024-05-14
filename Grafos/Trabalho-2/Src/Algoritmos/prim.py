35fbdfe9e1a68cb126cc0286792906e0bf4d35d1
ArturRSoda <artursodar@gmail.com>
2024-05-14 15:51:42 -0300

    grafos

--- Grafos/Trabalho-2/Src/Algoritmos/prim.py
+++ Grafos/Trabalho-2/Src/Algoritmos/prim.py
@@ -15,6 +15,8 @@ def prim(graph: Grafo) -> list[int]:
 
     # Define chave do vertice escolhido como 0
     K[r] = 0
+
+    C: list[bool] = [False for _ in range(graph.qtdVertices())]
     
     # Arvore binaria
     Q: BinaryHeap = BinaryHeap(K.copy())
@@ -23,18 +25,19 @@ def prim(graph: Grafo) -> list[int]:
     add: bool = True
     while (not Q.isEmpty()):
 
-
         if (add):
+            #Q: BinaryHeap = BinaryHeap([K[i] for i in range(len(K)) if (not C[i])])
             Q: BinaryHeap = BinaryHeap(K.copy())
             add = False
 
         # Seleciona vertice com menor chave
         u: int = K.index(Q.remove())+1
+        C[u-1] = True
         
         # Passa por todos os vizinhos de u
         for v in graph.vizinhos(u):
             # Se o peso da aresta (u, v) for menor que a chave de V
-            if ((graph.peso(u, v) < K[v-1])):
+            if ((not C[v-1]) and (graph.peso(u, v) < K[v-1])):
                 # Definimos u como antecessor de V
                 A[v-1] = u
                 # Definimos que a chave e V eh o peso de (u, v)
