15a01f881f4933d24e10bbc2c2d5becd4050fa05
ArturRSoda <artursodar@gmail.com>
2024-06-09 17:36:18 -0300

    Paradigmas: Trabalho 3 rearranjo main

--- Paradigmas_de_Programacao/Trabalhos/Trabalho_3/Src/main.pl
+++ Paradigmas_de_Programacao/Trabalhos/Trabalho_3/Src/main.pl
@@ -4,23 +4,15 @@
 % Le arquivos e monta matrizes de valores e regioes
 build_matriz(Path, ValuesMatriz, RegionsMatriz) :-
     atom_concat('Entradas/', Path, P1),
-    atom_concat(P1, '/numbers.txt', ValuesPath),
-    atom_concat(P1, '/regions.txt', RegionsPath),
 
-    open(ValuesPath, read, ValuesStr),
-    open(RegionsPath, read, RegionsStr),
+    atom_concat(P1, '/numbers.txt', ValuesPath), atom_concat(P1, '/regions.txt', RegionsPath),
+    open(ValuesPath, read, ValuesStr),           open(RegionsPath, read, RegionsStr),
+    read_matriz(ValuesStr, ValuesFile),          read_matriz(RegionsStr, RegionsFile),
+    close(ValuesStr),                            close(RegionsStr),
+    ValuesFile = [ValuesMatriz|_],               RegionsFile = [RegionsMatriz|_].
 
-    read_matriz(ValuesStr, ValuesFile),
-    read_matriz(RegionsStr, RegionsFile),
 
-    close(ValuesStr),
-    close(RegionsStr),
-
-    ValuesFile = [ValuesMatriz|_],
-    RegionsFile = [RegionsMatriz|_].
-
-read_matriz(Stream, []) :-
-    at_end_of_stream(Stream).
+read_matriz(Stream, []) :- at_end_of_stream(Stream).
 read_matriz(Stream, [H|T]) :-
     read(Stream, H),
     read_matriz(Stream, T).
